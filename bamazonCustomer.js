b5e1ed772520d46a3773f2f3471b7368b107f881
hfung8 <harrison.hok.fung@gmail.com>
2017-03-09 14:42:01 -0500

    redid homework

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -4,9 +4,7 @@ var inquirer = require("inquirer");
 var connection = mysql.createConnection({
 	host: "localhost",
 	port: 3306,
-
 	user: "root",
-
 	password: "",
 	database: "BAMAZON_DB"
 });
@@ -14,66 +12,62 @@ var connection = mysql.createConnection({
 connection.connect(function(err){
 	if (err) throw err;
 	console.log("connected as id " + connection.threadId);
-	startIt();
+	makeTable();
 });
 
-function startIt(){
-connection.query('SELECT * FROM Products', function(err,rows,fields){
-	if (err) throw err; 
-	console.log(rows);
-	start();
-});
-}
-
-var start = function(){
-	var questions = [{
-		type: "input",
-		name: "firstQuestion",
-		message: "What is the ID of the products that you would like to buy ?",
-		validate: function(value){
-		if (isNaN(value) == false){
-			return true;
-		} else {
-			return false;
+var makeTable = function(){
+connection.query('SELECT * FROM Products', function(err,res){
+	if (err) throw err;
+	for (var i = 0; i < res.length; i++){
+	console.log(res[i].ItemID+" || "+ res[i].ProductName+" || "+ res[i].DepartmentName+" || "+ res[i].Price+" || "+ res[i].StockQuantity+"\n");
 		}
-	}
-},
-	{
-		type: "input",
-		name: "secondQuestion",
-		message: "How many units of product would you like to buy?", 
-		validate: function(value){
-		if (isNaN(value) == false){
-			return true;
-		} else {
-			return false;
-		}
-	}	
-	}]
-
-inquirer.prompt(questions).then(function(answer){
-	var firstAnswer = answer.firstQuestion;
-	var secondAnswer = answer.secondQuestion;
-	var query = "SELECT ItemID,StockQuantity FROM Products WHERE ? = ItemID";
-	connection.query(query,firstAnswer,function (err,res){
-		for (var i = 0; i<res.length; i++){
-			if (res[i].StockQuantity >= secondAnswer){
-				var query = "UPDATE Products SET StockQuantity = StockQuantity - ? WHERE ? = ItemID";
-				connection.query(query,[secondAnswer,firstAnswer], function (err,res){
-				var query = "SElECT Price * ? FROM Products WHERE ? = ItemID";
-				connection.query(query,[secondAnswer,firstAnswer], function (err,res){
-				console.log(res);
-				})
-				})
-				} else { 
-				console.log("Insufficient Supply!");
-				start();		
-				}
-			}
-		})
+		promptCustomer(res);
 	})
 }
 
-
-
-
+var promptCustomer = function(res){
+	inquirer.prompt({
+		type: "input",
+		name: "choice",
+		message: "What would you like to purchase? Quit with [Q]",
+	}).then(function(answer){
+		console.log(answer);
+		var correct = false; 
+		if(answer.choice.toUpperCase()=="Q"){
+			process.exit();
+		}
+		for (var i = 0; i<res.length;i++){
+			if(res[i].ProductName==answer.choice){
+				correct=true;
+				var product=answer.choice;
+				var id = i;
+				inquirer.prompt({
+					type: "input",
+					name: "quant",
+					message: "How many units of product would you like to buy?", 
+					validate: function(value){
+					if (isNaN(value) == false){
+						return true;
+					} else {
+						return false;
+					}
+				}
+			}).then(function(answer){
+				if((res[id].StockQuantity-answer.quant)>0){
+					connection.query("UPDATE Products SET StockQuantity='"+(res[id].StockQuantity-answer.quant)+"'WHERE ProductName='"+product+"'", function(err,res2){
+						console.log("Product Bought!");
+						makeTable();
+					})
+				} else {
+					console.log("Not a valid selection!");
+					promptCustomer(res);
+				}
+			})
+		}
+	}
+if(i==res.length && correct==false){
+	console.log("Not a valid selection!")
+	promptCustomer(res);
+}
+	})
+		}
